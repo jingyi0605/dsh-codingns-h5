ddf85dd8de746eafb9df0a72226a97ed2a50b222
jingyi0605 <zj591227045@gmail.com>
2026-09-24 11:06:38 +0800

    配置公网控制站地址

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// 部署时可覆盖本文件；空字符串表示使用当前 dsh.codingns.com 同源 /api。
+// 如果 H5 域名没有把 /api 反代到控制站，可直接使用这里的公网控制站地址。
 window.__DSH_BOOTSTRAP_CONFIG__ = {
-  controlApiBaseUrl: "",
+  controlApiBaseUrl: "https://channel.codingns.com:1443",
 };
